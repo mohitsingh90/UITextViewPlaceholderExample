01aeeb0682704854d330e53c9e940b69f2e47b75
Mohit <mohitsingh@outlook.in>
2018-02-03 21:06:15 +0530

    Initial Commit

---
+++ UITextViewPlaceholderExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITextViewPlaceholderExample
+//
+//  Created by MOHIT SINGH on 03/02/18.
+//  Copyright © 2018 MOHIT SINGH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
